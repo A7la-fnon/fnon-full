e3c1abfa52397ba9acce89240215b28ea2bcfed6
ahmedto <ahmed.to2004@gmail.com>
2020-10-07 01:18:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function ws_basic(c,a,b){this.go=function(d){b.find("ul").stop(true).animate({left:(d?-d+"00%":(/Safari/.test(navigator.userAgent)?"0%":0))},c.duration,"easeInOutExpo");return d}};
+jQuery("#wowslider-container1").wowSlider({effect:"basic",prev:"",next:"",duration:20*100,delay:20*100,width:207,height:116,autoPlay:true,playPause:false,stopOnHover:false,loop:false,bullets:true,caption:true,captionEffect:"slide",controls:true,onBeforeStep:0,images:0});
---
+++ slide.hide.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+$(".sec_section,.sec_section2,.sec_section3,.sec_section4").click(function () {
+$(".wowslider-container2").show("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".sec_section,.sec_section2,.sec_section3,.sec_section4").click(function () {
+$(".wowslider-container1,.wowslider-container3,.wowslider-container4").hide("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".first_sction,.first_sction2,.first_sction3,.first_sction4").click(function () {
+$(".wowslider-container1").show("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".first_sction,.first_sction2,.first_sction3,.first_sction4").click(function () {
+$(".wowslider-container3,.wowslider-container2,.wowslider-container4").hide("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".sction_3,.sction_32,.sction_33,.sction_34").click(function () {
+$(".wowslider-container3").show("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".sction_3,.sction_32,.sction_33,.sction_34").click(function () {
+$(".wowslider-container1,.wowslider-container2,.wowslider-container4").hide("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".section_4,.section_42,.section_43,.section_44").click(function () {
+$(".wowslider-container4").show("slow");
+	});
+});
+
+$(document).ready(function(){
+$(".section_4,.section_42,.section_43,.section_44").click(function () {
+$(".wowslider-container1,.wowslider-container2,.wowslider-container3").hide("slow");
+	});
+});
+
+
+		$(document).ready(function(){
+$("#signin").click(function () {
+$("#memarrea").show("slow");
+});
+		});
+		
+		
+				$(document).ready(function(){
+$("#close_memarea").click(function () {
+$("#memarrea").hide("slow");
+});
+		});
+		
+		
+		
+				$(document).ready(function(){
+ $(".to_top").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
+		});
